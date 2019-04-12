104bc783178a4e7ee9e5c3114afb6d600ac45fb1
Jose <jose.zdy@gmail.com>
2019-04-12 17:40:20 -0500

    create test_utils.py file, use it in test_models

--- pennydjango/rentals/tests/test_models.py
+++ pennydjango/rentals/tests/test_models.py
@@ -1,36 +1,18 @@
-from django.test import TestCase
-from django.contrib.auth import get_user_model
-
+from rentals.tests.test_utils import SimpleRentTest
 from rentals.models import RentProperty
 
 
-class RentPropertyTestCase(TestCase):
-    def setUp(self):
-        self.test_user = get_user_model().objects.create_user(
-            username='test_pub',
-            password='alalalalong'
-        )
-        RentProperty.objects.create(
-            publisher=self.test_user,
-            price=999,
-            contact='bob at 018000-marley',
-            address='420 love street',
-            latitude=40.786317,
-            longitude=-73.962212,
-            about='no rooftop so you can view the stars at night',
-            bedrooms=3,
-            baths=2,
-        )
+class RentPropertyTestCase(SimpleRentTest):
 
     def test_rent_property(self):
-        rent_prop = RentProperty.objects.get(publisher=self.test_user)
+        rent_prop = RentProperty.objects.first()
         assert rent_prop.id
         assert rent_prop.contact
         assert rent_prop.address
         assert rent_prop.latitude
         assert rent_prop.longitude
         assert rent_prop.about
-        assert rent_prop.bedrooms == 3
-        assert rent_prop.baths == 2
+        assert rent_prop.bedrooms > 0
+        assert rent_prop.baths > 0
         assert rent_prop.pets_allowed
         assert rent_prop.amenities is ""
