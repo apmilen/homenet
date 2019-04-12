57b830f684374f70927b80d85e0b9cd30a0b3a68
juan <juandgoc@gmail.com>
2019-04-11 19:36:39 -0500

    user model

---
+++ pennydjango/penny/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser, UserManager
+
+from .model_utils import BaseModel
+
+
+class CaseInsensitiveUserManager(UserManager):
+    def get_by_natural_key(self, username):
+        return self.get(username__iexact=username)
+
+
+class User(AbstractUser, BaseModel):
+    objects = CaseInsensitiveUserManager()
+    # id = models.UUIDField
+    # username
+    # password
+    # email
+    # first_name
+    # last_name
+    # is_active
+    # is_staff
+    # is_superuser
+    # last_login
+    # date_joined
+
