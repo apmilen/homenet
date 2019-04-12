2bda14253d0e33dd4f48674170792b584ed16863
juan <juandgoc@gmail.com>
2019-04-11 20:38:39 -0500

    added user type

--- pennydjango/penny/schema.py
+++ pennydjango/penny/schema.py
@@ -1,5 +1,14 @@
 import graphene
+from graphene_django.types import DjangoObjectType
+
 import rentals.schema
+
+from .models import User
+
+
+class CategoryType(DjangoObjectType):
+    class Meta:
+        model = User
 
 
 class Query(rentals.schema.Query, graphene.ObjectType):
