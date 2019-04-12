af7495b2891acb8b35f696dc760409be2c3b85c2
Jose <jose.zdy@gmail.com>
2019-04-12 17:48:19 -0500

    simplify test

--- pennydjango/rentals/tests/test_models.py
+++ pennydjango/rentals/tests/test_models.py
@@ -1,18 +1,17 @@
 from rentals.tests.test_utils import SimpleRentTest
-from rentals.models import RentProperty
 
 
 class RentPropertyTestCase(SimpleRentTest):
 
     def test_rent_property(self):
-        rent_prop = RentProperty.objects.first()
+        rent_prop = self.property1
         assert rent_prop.id
         assert rent_prop.contact
         assert rent_prop.address
         assert rent_prop.latitude
         assert rent_prop.longitude
         assert rent_prop.about
-        assert rent_prop.bedrooms > 0
-        assert rent_prop.baths > 0
+        assert rent_prop.bedrooms == 3
+        assert rent_prop.baths == 2
         assert rent_prop.pets_allowed
         assert rent_prop.amenities is ""
