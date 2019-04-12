e3c42e9e50a39607ca8cbbccb6cab5bd8186a55a
Jose <jose.zdy@gmail.com>
2019-04-12 16:59:52 -0500

    django test for rentals models

--- pennydjango/rentals/tests/test_models.py
+++ pennydjango/rentals/tests/test_models.py
@@ -1,21 +1,36 @@
-import pytest
+from django.test import TestCase
+from django.contrib.auth import get_user_model
 
-from mixer.backend.django import mixer
-
-
-# We need to do this so that writing to the DB is possible in our tests.
-pytestmark = pytest.mark.django_db
+from rentals.models import RentProperty
 
 
-def test_rent_property():
-    rent_prop = mixer.blend('rentals.RentProperty')
-    assert rent_prop.id
-    assert rent_prop.contact
-    assert rent_prop.address
-    assert rent_prop.latitude
-    assert rent_prop.longitude
-    assert rent_prop.about is None
-    assert rent_prop.bedrooms
-    assert rent_prop.baths
-    assert rent_prop.pets_allowed
-    assert rent_prop.amenities
+class RentPropertyTestCase(TestCase):
+    def setUp(self):
+        self.test_user = get_user_model().objects.create_user(
+            username='test_pub',
+            password='alalalalong'
+        )
+        RentProperty.objects.create(
+            publisher=self.test_user,
+            price=999,
+            contact='bob at 018000-marley',
+            address='420 love street',
+            latitude=40.786317,
+            longitude=-73.962212,
+            about='no rooftop so you can view the stars at night',
+            bedrooms=3,
+            baths=2,
+        )
+
+    def test_rent_property(self):
+        rent_prop = RentProperty.objects.get(publisher=self.test_user)
+        assert rent_prop.id
+        assert rent_prop.contact
+        assert rent_prop.address
+        assert rent_prop.latitude
+        assert rent_prop.longitude
+        assert rent_prop.about
+        assert rent_prop.bedrooms == 3
+        assert rent_prop.baths == 2
+        assert rent_prop.pets_allowed
+        assert rent_prop.amenities is ""
