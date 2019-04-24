11f2e78786bfd978ade09f35744f43adf89f53e7
JoseB <jose.zdy@gmail.com>
2019-04-24 12:35:00 -0500

    create schedule page

--- pennydjango/ui/urls.py
+++ pennydjango/ui/urls.py
@@ -2,12 +2,13 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
-from ui.views.pages import Home
+from ui.views.pages import Home, Schedule
 from ui.views.accounts import Signup
 
 urlpatterns = [
+    path('', Home.as_view()),
+    path('schedule', Schedule.as_view(), name='schedule'),
     path('signup', Signup.as_view(), name='signup'),
-    path('', Home.as_view())
 ]
 
 if settings.DEBUG:
--- pennydjango/ui/views/pages.py
+++ pennydjango/ui/views/pages.py
@@ -1,6 +1,8 @@
 from django.views.generic.list import ListView
 
-from rentals.models import RentProperty
+from rentals.models import RentProperty, Availability
+from rentals.forms import AvailabilityForm
+
 from ui.views.base_views import PublicReactView, BaseContextMixin
 
 
@@ -14,3 +16,21 @@ class Home(BaseContextMixin, ListView):
     title = 'Home'
     template_name = 'ui/home.html'
     model = RentProperty
+
+
+class Schedule(BaseContextMixin, ListView):
+    title = 'Schedule'
+
+    def get_queryset(self):
+        return Availability.objects.filter(agent=self.request.user)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'] = AvailabilityForm()
+        return context
+
+    def post(self, request, *args, **kwargs):
+        form = AvailabilityForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return super().get(request, *args, **kwargs)
