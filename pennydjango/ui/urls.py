276e32d820345234d6f643df72ee2ad4a3c45dbd
juan <juandgoc@gmail.com>
2019-04-25 10:06:39 -0500

    templates tweaks

--- pennydjango/ui/urls.py
+++ pennydjango/ui/urls.py
@@ -1,6 +1,4 @@
 from django.urls import path
-from django.conf import settings
-from django.conf.urls.static import static
 
 from ui.views.pages import Home
 from ui.views.accounts import Signup
@@ -9,9 +7,3 @@ urlpatterns = [
     path('signup', Signup.as_view(), name='signup'),
     path('', Home.as_view())
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(
-        settings.MEDIA_URL,
-        document_root=settings.MEDIA_ROOT
-    )
