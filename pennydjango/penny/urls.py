0e3124cf5b84fbccaf9f7ff42b122e70fa03682d
juan <juandgoc@gmail.com>
2019-04-25 10:05:08 -0500

    moved static serve to main urls file

--- pennydjango/penny/urls.py
+++ pennydjango/penny/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from django.views.decorators.csrf import csrf_exempt
@@ -27,3 +29,10 @@ urlpatterns = [
     path('gql', csrf_exempt(GraphQLView.as_view())),
     path('', include("ui.urls"))
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
