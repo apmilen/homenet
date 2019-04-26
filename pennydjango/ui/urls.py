8a4b993afbd7d58793cb2e9af987cef79d1c1493
JoseB <jose.zdy@gmail.com>
2019-04-25 23:16:59 -0500

    create profile view

--- pennydjango/ui/urls.py
+++ pennydjango/ui/urls.py
@@ -3,11 +3,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 from ui.views.pages import Home
-from ui.views.accounts import Signup
+from ui.views.accounts import Signup, UserProfile
 
 urlpatterns = [
     path('signup', Signup.as_view(), name='signup'),
-    path('', Home.as_view())
+    path('', Home.as_view()),
+    path('user/<username>/', UserProfile.as_view(), name="userprofile")
 ]
 
 if settings.DEBUG:
--- pennydjango/ui/views/accounts.py
+++ pennydjango/ui/views/accounts.py
@@ -1,6 +1,8 @@
 from django.contrib.auth import login
 from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404
 from django.views.generic.edit import CreateView
+from django.views.generic.detail import DetailView
 
 from penny.models import User
 from penny.forms import CustomUserCreationForm
@@ -16,3 +18,10 @@ class Signup(CreateView):
         self.object = form.save()
         login(self.request, self.object)
         return HttpResponseRedirect(self.get_success_url())
+
+
+class UserProfile(DetailView):
+    model = User
+
+    def get_object(self):
+        return get_object_or_404(User, username=self.kwargs.get('username'))
