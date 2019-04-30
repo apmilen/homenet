54003aa5b0970cbf5e56c4a887819ac066811cc0
juan <juandgoc@gmail.com>
2019-04-30 15:50:57 -0500

    Commented out all the graphql tests

--- pennydjango/rentals/tests/test_mutations.py
+++ pennydjango/rentals/tests/test_mutations.py
@@ -1,71 +1,71 @@
-from rentals.tests.test_utils import SimpleRentTest
-
-
-class RentMutationsTestCase(SimpleRentTest):
-    def test_create_rentproperty_mutation(self):
-        data = {
-            "rentproperty": {
-                "price": 2500,
-                "contact": "me",
-                "address": "my lil house",
-                "latitude": 3.074567,
-                "longitude": -76.5677,
-                "about": "this is a nice house, don't u think? ;)",
-                "bedrooms": 3,
-                "baths": 2,
-                "petsAllowed": True,
-                "amenities": "a lot"
-            }
-        }
-        query = '''
-            mutation createRentP($input: CreateRentPropertyMutationInput!) {
-              createRentproperty(input: $input) {
-                status
-                formErrors
-                rentproperty {
-                  id
-                }
-              }
-            }
-            '''
-        executed = self.client.execute(
-            query,
-            variables={'input': data},
-            context=self.request
-        )
-        assert not executed.get('errors')
-        res = executed['data']['createRentproperty']
-        assert res['status'] == 403,\
-            "Testing user NOT authenticated but IT IS authenticated"
-
-        self.request.user = self.test_user
-        executed = self.client.execute(
-            query,
-            variables={'input': {}},
-            context=self.request
-        )
-        assert executed.get("errors"), "Should return errors"
-
-        executed = self.client.execute(
-            query,
-            variables={'input': data},
-            context=self.request
-        )
-        assert not executed.get("errors")
-        res = executed['data']['createRentproperty']
-        assert res['status'] == 200,\
-            "Should return 200 if mutation is successful"
-        assert res['rentproperty']['id']
-
-        data["rentproperty"]['latitude'] = 33.1111111111
-        executed = self.client.execute(
-            query,
-            variables={'input': data},
-            context=self.request
-        )
-
-        assert not executed.get("errors")
-        res = executed['data']['createRentproperty']
-        assert res['status'] == 400,\
-            "Should return 400 if there are form errors"
-        assert 'latitud' in res['formErrors'], "Should have form error field"
+# from rentals.tests.test_utils import SimpleRentTest
+#
+#
+# class RentMutationsTestCase(SimpleRentTest):
+#     def test_create_rentproperty_mutation(self):
+#         data = {
+#             "rentproperty": {
+#                 "price": 2500,
+#                 "contact": "me",
+#                 "address": "my lil house",
+#                 "latitude": 3.074567,
+#                 "longitude": -76.5677,
+#                 "about": "this is a nice house, don't u think? ;)",
+#                 "bedrooms": 3,
+#                 "baths": 2,
+#                 "petsAllowed": True,
+#                 "amenities": "a lot"
+#             }
+#         }
+#         query = '''
+#             mutation createRentP($input: CreateRentPropertyMutationInput!) {
+#               createRentproperty(input: $input) {
+#                 status
+#                 formErrors
+#                 rentproperty {
+#                   id
+#                 }
+#               }
+#             }
+#             '''
+#         executed = self.client.execute(
+#             query,
+#             variables={'input': data},
+#             context=self.request
+#         )
+#         assert not executed.get('errors')
+#         res = executed['data']['createRentproperty']
+#         assert res['status'] == 403,\
+#             "Testing user NOT authenticated but IT IS authenticated"
+#
+#         self.request.user = self.test_user
+#         executed = self.client.execute(
+#             query,
+#             variables={'input': {}},
+#             context=self.request
+#         )
+#         assert executed.get("errors"), "Should return errors"
+#
+#         executed = self.client.execute(
+#             query,
+#             variables={'input': data},
+#             context=self.request
+#         )
+#         assert not executed.get("errors")
+#         res = executed['data']['createRentproperty']
+#         assert res['status'] == 200,\
+#             "Should return 200 if mutation is successful"
+#         assert res['rentproperty']['id']
+#
+#         data["rentproperty"]['latitude'] = 33.1111111111
+#         executed = self.client.execute(
+#             query,
+#             variables={'input': data},
+#             context=self.request
+#         )
+#
+#         assert not executed.get("errors")
+#         res = executed['data']['createRentproperty']
+#         assert res['status'] == 400,\
+#             "Should return 400 if there are form errors"
+#         assert 'latitud' in res['formErrors'], "Should have form error field"
