1023b5282a8ffa3f20d04a544461399ed8a4c1c2
JoseB <jose.zdy@gmail.com>
2019-05-06 12:28:31 -0500

    improve styles

--- pennydjango/rentals/forms.py
+++ pennydjango/rentals/forms.py
@@ -4,6 +4,11 @@ from .models import RentProperty
 
 
 class CreateRentPropertyForm(forms.ModelForm):
+    is_listed = forms.BooleanField(
+        label="Show listing",
+        help_text="Uncheck to make it private (draft)."
+    )
+
     class Meta:
         model = RentProperty
         exclude = ("publisher", )
