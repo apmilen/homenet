8404d9f9ac13ae66a84e10aadd7cbc77149c0348
JoseB <jose.zdy@gmail.com>
2019-05-16 04:34:13 -0500

    allow user creation

--- pennydjango/js/pages/users.js
+++ pennydjango/js/pages/users.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import {
-    Row, Card, Button
+    Row, Card, FormControl, Button, Alert
 } from 'react-bootstrap'
 
 
@@ -28,18 +28,72 @@ class UserCard extends React.Component {
 
 
 class Users extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            email: '',
+            user_type: props.user_types[0][0],
+            users: props.users,
+            errors: '',
+        }
+    }
+    changeField(e) {
+        this.setState({[e.target.id]: e.target.value})
+    }
+    postUser() {
+        const {email, user_type} = this.state
+        const post_data = {type: 'NEW_USER', email, user_type}
+
+        $.post('', post_data, (resp) => {
+            if (resp.success) {
+                const new_users = this.state.users.concat(resp.new_user)
+                this.setState({
+                    email: '',
+                    users: new_users,
+                    errors: ''
+                })
+            } else {
+                this.setState({errors: resp.details})
+            }
+        })
+    }
     render() {
-        const {users} = this.props
+        const {user_types} = this.props
+        const {users} = this.state
+
         return (
             <div>
                 <Row className="justify-content-center" style={{ padding: 10 }}>
                     {users.map(user =>
                         <UserCard {...user} />
                     )}
-                    <Card style={{ width: '18rem', margin: 5 }} className="overlay-parent">
-                        <a href="new/" className="overlay"></a>
-                        <i style={{ fontSize: '12rem', margin: 'auto' }}
-                           class="material-icons">add_circle_outline</i>
+                    <Card style={{ width: '18rem', margin: 5 }}>
+                        <div style={{ margin: 'auto', padding: '60px 0' }}>
+                            {this.state.errors &&
+                                <Alert variant='danger' style={{textAlign: 'center', maxWidth: '13rem'}}>
+                                    {this.state.errors}
+                                </Alert>
+                            }
+                            <FormControl id='email'
+                                         type='email'
+                                         value={this.state.email}
+                                         placeholder='Enter email'
+                                         onChange={::this.changeField} />
+                            <br/>
+                            <FormControl id='user_type'
+                                         as='select'
+                                         value={this.state.user_type}
+                                         onChange={::this.changeField} >
+                                {user_types.map(user_type =>
+                                    <option value={user_type[0]}>{user_type[1]}</option>
+                                )}
+                            </FormControl>
+                            <br/>
+                            <Button variant="primary" onClick={::this.postUser}>
+                                Send invitation
+                            </Button>
+
+                        </div>
                     </Card>
                 </Row>
             </div>
