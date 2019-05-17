75667430de393b1eeada323c41546db8eb10d132
JoseB <jose.zdy@gmail.com>
2019-05-16 21:04:02 -0500

    more fields for user creation

--- pennydjango/js/pages/users.js
+++ pennydjango/js/pages/users.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import {
-    Row, Card, FormControl, Button, Alert
+    Row, Card, FormControl, Button, Alert, InputGroup
 } from 'react-bootstrap'
 
 
@@ -12,15 +12,15 @@ class UserCard extends React.Component {
 
         return (
             <Card style={{ width: '18rem', margin: 5 }}>
-                <Card.Header as="h3">{user.first_name || 'Unnamed'}</Card.Header>
                 <div style={{ padding: 20 }}>
                     <div class="circle-avatar" style={{ backgroundImage: `url(${user.avatar_url})` }}></div>
                 </div>
-                <Card.Body>
-                    <Card.Title>@{user.username}</Card.Title>
-                    <Card.Subtitle className="mb-2 text-muted">{user.email || 'no email'}</Card.Subtitle>
-                    <Card.Subtitle className="mb-2 text-muted">{user.user_type_str}</Card.Subtitle>
-                </Card.Body>
+                <div style={{ padding: '0 1.9rem' }}>
+                <h3>{user.first_name || 'Unnamed'}</h3>
+                <Card.Title>@{user.username}</Card.Title>
+                <Card.Subtitle className="text-muted">{user.email || 'no email'}</Card.Subtitle>
+                <Card.Subtitle className="">{user.user_type_str}</Card.Subtitle>
+                </div>
             </Card>
         )
     }
@@ -31,6 +31,8 @@ class Users extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
+            name: '',
+            username: '',
             email: '',
             user_type: props.user_types[0][0],
             users: props.users,
@@ -41,13 +43,18 @@ class Users extends React.Component {
         this.setState({[e.target.id]: e.target.value})
     }
     postUser() {
-        const {email, user_type} = this.state
-        const post_data = {type: 'NEW_USER', email, user_type}
+        const {name, username, email, user_type} = this.state
+        const post_data = {
+            type: 'NEW_USER',
+            name, username, email, user_type
+        }
 
         $.post('', post_data, (resp) => {
             if (resp.success) {
                 const new_users = this.state.users.concat(resp.new_user)
                 this.setState({
+                    name: '',
+                    username: '',
                     email: '',
                     users: new_users,
                     errors: ''
@@ -68,19 +75,34 @@ class Users extends React.Component {
                         <UserCard {...user} />
                     )}
                     <Card style={{ width: '18rem', margin: 5 }}>
-                        <div style={{ margin: 'auto', padding: '60px 0' }}>
+                        <div style={{ margin: 'auto', padding: '60px 0', width: '80%' }}>
                             {this.state.errors &&
-                                <Alert variant='danger' style={{textAlign: 'center', maxWidth: '13rem'}}>
+                                <Alert variant='danger' style={{textAlign: 'center', width: '100%'}}>
                                     {this.state.errors}
                                 </Alert>
                             }
-                            <FormControl id='email'
+                            <h4>Add person:</h4>
+                            <FormControl id='name' className="my-2"
+                                         type='text'
+                                         value={this.state.name}
+                                         placeholder='Name'
+                                         onChange={::this.changeField} />
+                            <InputGroup className="my-2">
+                                <InputGroup.Prepend>
+                                    <InputGroup.Text>@</InputGroup.Text>
+                                </InputGroup.Prepend>
+                                <FormControl id='username'
+                                             type='text'
+                                             value={this.state.username}
+                                             placeholder='username'
+                                             onChange={::this.changeField} />
+                            </InputGroup>
+                            <FormControl id='email' className="my-2"
                                          type='email'
                                          value={this.state.email}
                                          placeholder='Enter email'
                                          onChange={::this.changeField} />
-                            <br/>
-                            <FormControl id='user_type'
+                            <FormControl id='user_type' className="my-2"
                                          as='select'
                                          value={this.state.user_type}
                                          onChange={::this.changeField} >
@@ -88,7 +110,6 @@ class Users extends React.Component {
                                     <option value={user_type[0]}>{user_type[1]}</option>
                                 )}
                             </FormControl>
-                            <br/>
                             <Button variant="primary" onClick={::this.postUser}>
                                 Send invitation
                             </Button>
