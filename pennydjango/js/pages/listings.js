f72ddf8241b5902ea37c376ce04747f15747120e
JoseB <jose.zdy@gmail.com>
2019-06-05 23:23:29 -0500

    pagination for agent listings

--- pennydjango/js/pages/listings.js
+++ pennydjango/js/pages/listings.js
@@ -30,12 +30,22 @@ class Listings extends React.Component {
                 draft_listings: false,
                 date_available: '',
             },
-            'listings': [],
+            listings: [],
+            more_listings_link: null,
         }
+    }
+    moreListings() {
+        $.get(this.state.more_listings_link, (resp) =>
+            this.setState({
+                listings: this.state.listings.concat(resp.results),
+                more_listings_link: resp.next
+            })
+        )
     }
     render() {
         const {constants, endpoint} = this.props
-        const {listings, filters} = this.state
+        const {listings, filters, more_listings_link} = this.state
+        const last_listing = listings.length > 0 && listings.slice(-1)[0]
 
         return [
             <div className="row justify-content-center">
@@ -50,10 +60,27 @@ class Listings extends React.Component {
                         <div className= "col">
                             {listings.map(listing => [
                                 <ListingComponent key={listing.short_id} listing={listing} constants={constants}/>,
-                                <hr key={`${listing.short_id}-hr`} className= "listings-hr" />
+                                listing.short_id != last_listing.short_id &&
+                                    <hr key={`${listing.short_id}-hr`} className= "listings-hr" />
                             ])}
-                            {listings.length == 0 &&
+                            {listings.length == 0 ?
                                 <center><h4>No listings found</h4></center>
+                            :
+                                <div className="row justify-content-center">
+                                    {more_listings_link ?
+                                        <button onClick={::this.moreListings} className="btn btn-outline-primary" style={{padding: 5, margin: 10}}>
+                                            Load more...
+                                        </button>
+                                    :
+                                        <div style={{width: '100%', height: 30, textAlign: 'center'}}>
+                                            <div style={{borderBottom: '1px solid gray', height: 12, width: '95%', margin: 'auto'}}>
+                                                <span style={{fontSize: '1em', backgroundColor: 'white', padding: '0 10px'}}>
+                                                    End of results
+                                                </span>
+                                            </div>
+                                        </div>
+                                    }
+                                </div>
                             }
                         </div>
                     </div>
