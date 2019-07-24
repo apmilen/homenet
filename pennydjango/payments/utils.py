6cd9210e79fece7dbbcd5301fff9cb6fd0dc4e8c
Caroguza <canamag@gmail.com>
2019-07-24 10:00:59 -0500

    Amount paid by the user is ruunded to the next higher integer.

--- pennydjango/payments/utils.py
+++ pennydjango/payments/utils.py
@@ -1,8 +1,9 @@
 from decimal import Decimal
+from math import ceil
 
 
 def get_amount_plus_fee(amount):
     stripe_fixed_comission = Decimal(0.30)
     total_paid = (amount+stripe_fixed_comission)/Decimal(1-0.029)
-    total_paid = total_paid
+    total_paid = ceil(total_paid)
     return total_paid
