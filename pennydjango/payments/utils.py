cbe8cde701d959a7a90a0bbb86055d7aec5cd222
Caroguza <canamag@gmail.com>
2019-07-24 11:17:07 -0500

    Amount paid plus Stripe fee calculation improved.

--- pennydjango/payments/utils.py
+++ pennydjango/payments/utils.py
@@ -1,8 +1,8 @@
 from decimal import Decimal
-
+from math import ceil
 
 def get_amount_plus_fee(amount):
     stripe_fixed_comission = Decimal(0.30)
     total_paid = (amount+stripe_fixed_comission)/Decimal(1-0.029)
-    total_paid = round(total_paid, 2)
+    total_paid = ceil(total_paid * 100)
     return total_paid
