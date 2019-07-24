9a66844a4d6454352534dd2be553abc34c2ebf39
juan <juandgoc@gmail.com>
2019-07-24 15:37:18 -0500

    removed secret keys test stripe

--- pennydjango/config/settings.py
+++ pennydjango/config/settings.py
@@ -145,6 +145,11 @@ MAILGUN_API_KEY = _PLACEHOLDER_FOR_UNSET
 MAP_KEY = _PLACEHOLDER_FOR_UNSET
 
 
+# Stripe Key Settings
+STRIPE_SECRET_KEY = _PLACEHOLDER_FOR_UNSET
+STRIPE_PUBLISHABLE_KEY = _PLACEHOLDER_FOR_UNSET
+
+
 ################################################################################
 ### Internationalization & Formatting Settings
 ################################################################################
@@ -383,10 +388,6 @@ if PENNY_ENV == 'PROD':
 check_django_invariants()
 chown_django_folders()
 STATUS_LINE = log_django_status_line()
-
-# Stripe Key Settings
-STRIPE_SECRET_KEY = 'sk_test_JMvIu8Y2VUpRtgkc1F2PJyEp00KdF6u0pE'
-STRIPE_PUBLISHABLE_KEY = 'pk_test_pwFJg5nhA95y3myECsDvuhxW003FKbgc7E'
 
 
 # Application name
