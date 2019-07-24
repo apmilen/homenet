5a364537129b03af49746c0cd94d7eaf82bc84cb
JoseB <jose.zdy@gmail.com>
2019-07-24 15:29:20 -0500

    collection detail

--- pennydjango/listing_collections/views.py
+++ pennydjango/listing_collections/views.py
@@ -1,6 +1,7 @@
+from django.conf import settings
 from django.urls import reverse
 from django.http import Http404
-from django.views.generic import ListView, DeleteView
+from django.views.generic import ListView, DeleteView, DetailView
 
 from penny.mixins import AgentRequiredMixin
 from listing_collections.models import Collection
@@ -25,3 +26,14 @@ class CollectionDelete(AgentRequiredMixin, DeleteView):
         if not obj.created_by == self.request.user:
             raise Http404
         return obj
+
+
+class CollectionDetail(DetailView):
+    model = Collection
+    template_name = 'listing_collections/collection_detail.html'
+    slug_field = 'id__startswith'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['map_key'] = settings.MAP_KEY
+        return context
