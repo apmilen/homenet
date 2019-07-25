0e7b5780caf5cc68b40f635de38db5f21481f2f3
JoseB <jose.zdy@gmail.com>
2019-07-25 08:52:07 -0500

    send collection link via email modal

--- pennydjango/listing_collections/views.py
+++ pennydjango/listing_collections/views.py
@@ -1,7 +1,9 @@
 from django.conf import settings
 from django.urls import reverse
-from django.http import Http404
+from django.http import Http404, JsonResponse, HttpResponseForbidden
+from django.core.mail import send_mail
 from django.views.generic import ListView, DeleteView, DetailView
+from django.template.loader import render_to_string
 
 from penny.mixins import AgentRequiredMixin
 from listing_collections.models import Collection
@@ -39,3 +41,41 @@ class CollectionDetail(DetailView):
         context = super().get_context_data(**kwargs)
         context['map_key'] = settings.MAP_KEY
         return context
+
+    def post(self, request, *args, **kwargs):
+        # Collections are visible for non-logged users,
+        # but they are not allowed to send emails
+        if not request.user.is_authenticated:
+            return HttpResponseForbidden()
+
+        to_email = request.POST.get('email', '')
+        note = request.POST.get('note', '')
+
+        minimum_validation = all(
+            required_char in to_email for required_char in ("@", ".")
+        )
+        if not minimum_validation:
+            return JsonResponse({
+                'status': 200,
+                'success': False,
+                'message': "Invalid email address"
+            })
+
+        collection = self.get_object()
+        path = reverse('collections:detail', args=[collection.short_id])
+
+        subject = f"Listing collection: {collection.name}"
+        body = render_to_string(
+            "email/collections/share_collection_link.txt",
+            context={
+                'url': f"{settings.BASE_URL}{path}",
+                'note': note
+            }
+        )
+        send_mail(subject, body, settings.DEFAULT_FROM_EMAIL, [to_email])
+
+        return JsonResponse({
+            'status': 200,
+            'success': True,
+            'message': "Email sent successfully"
+        })
